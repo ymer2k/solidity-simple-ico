e33a32501d80a052c55340c102d2014e7d3aa250
Ymer2k <dr_sanoj@hotmail.com>
2022-04-02 14:19:08 +0200

    done

--- contracts/Ico.sol
+++ contracts/Ico.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.13;
 
 contract Ico {
-    uint public tokenPrice = 1;
-    address public owner;
+    uint public tokenPrice = 10;
+    address payable public owner;
 
     modifier onlyOwner() {
         require(msg.sender == owner);
@@ -11,22 +11,26 @@ contract Ico {
     }
 
     constructor () {
-        owner = msg.sender; // the publisher of the contract becomes the owner
+        owner = payable(msg.sender); // the publisher of the contract becomes the owner
     }
 
-    // constructor (address payable _wallet) { 
-    //     owner = _wallet; // the publisher of the contract becomes the owner
-    // }
+    // @notice Will receive any eth sent to the contract
+    receive() external payable {
+        buyTokens();
+    }
 
     mapping(address => uint) public balances; // keep track of how many tokens each address should have
 
-    function buyTokens(uint _ethAmount) public { // We should change this to msg.value later
-        uint amountOfJnsTokens = calculateAmountOfJnsTokens(_ethAmount);
+    function buyTokens() public payable {
+        uint amountOfJnsTokens = calculateAmountOfJnsTokens(msg.value);
         balances[msg.sender] = amountOfJnsTokens;
+        // Give tokens to contract owner.
+        owner.transfer(msg.value);
     }
 
-    function calculateAmountOfJnsTokens(uint _ethAmount) view internal returns(uint) {
-        uint jnsTokens = _ethAmount * tokenPrice;
+    function calculateAmountOfJnsTokens(uint _weiAmount) view internal returns(uint) {
+        uint etherAmount = _weiAmount / (10**18); // convert to eth
+        uint jnsTokens = etherAmount * tokenPrice; // Default is, 1 eth gives 10 tokens.
         return jnsTokens;
     }
 
--- test/integration/payment.js
+++ test/integration/payment.js
@@ -12,24 +12,25 @@ contract('payment', (accounts) =>{
         assert.equal(parseInt(value),expectedPrice) // ParseInt converts a string to an integear
     })
 
-    it("Calculate correct eth to token amount", async () =>{
-        const instance = await Ico.deployed()
-        const ethereumAmount = 10
-        const expectedPrice = 10
-        const expectedTokensBought = 100
-        // set the token price
-        await instance.changeTokenPrice(expectedPrice)
+    // This test doesn't work anymore since I changed how we pay for tokens but keep it for future reference
+    // it("Calculate correct eth to token amount", async () =>{
+    //     const instance = await Ico.deployed()
+    //     const ethereumAmount = 10
+    //     const expectedPrice = 10
+    //     const expectedTokensBought = 100
+    //     // set the token price
+    //     await instance.changeTokenPrice(expectedPrice)
 
-        //Buy tokens
-        await instance.buyTokens(ethereumAmount)
-        // Get the address that bought tokens.
-        // Just use accounts[0] that seem to be the default address!
+    //     //Buy tokens
+    //     await instance.buyTokens(ethereumAmount)
+    //     // Get the address that bought tokens.
+    //     // Just use accounts[0] that seem to be the default address!
 
-        // Check that the address got the correct amount of tokens.
-        const value = await instance.balances(accounts[0])
-        assert.equal(parseInt(value),expectedTokensBought)
+    //     // Check that the address got the correct amount of tokens.
+    //     const value = await instance.balances(accounts[0])
+    //     assert.equal(parseInt(value),expectedTokensBought)
 
-    })
+    // })
   
     it("should not allow other addresses than owner to change the tokenPrice", async function () {
         // This test will test that we get an error when notOwner tries to change the token price!
@@ -50,16 +51,69 @@ contract('payment', (accounts) =>{
         }
     });
 
-    // //Lets write a test that checks that the deployer of the contract is the owner.
-    // it("The deployer of the contract is the owner", async () =>{
-    //     const instance = await Ico.deployed(accounts[0], {from:accounts[0]})
-    //     const value = await instance.owner.call()
-    //     const owner = accounts[0]
+    //Lets write a test that checks that the deployer of the contract is the owner.
+    it("The deployer of the contract is the owner", async () =>{
+        //const instance = await Ico.deployed(accounts[0], {from:accounts[0]})
+        const instance = await Ico.deployed()
 
-    //     assert.equal(parseInt(value),owner)
+        const value = await instance.owner.call()
+        const owner = accounts[0]
+  
+        assert.equal(parseInt(value),owner)
+    })
+
+    // // Test buy tokens and check token balance.
+    // it("Sender should receive correct amount of tokens after sending eth", async () =>{
+    //     const instance = await Ico.deployed()
+    //     const ethereumAmount = 10
+    //     const expectedTokensBought = 10;
+    //     // Should call the fallback function
+    //     //and thus also the buyTokens function
+    //     await instance.send(ethereumAmount, {from: accounts[1]}) 
+
+    //     //Check the balance.
+    //     const value = await instance.balances(accounts[1])
+    //     assert.equal(parseInt(value),expectedTokensBought)
+
     // })
 
+    //Test buy tokens and check eth balance of owner and sender
 
 
    // This test should suck my dick
+})
+
+
+
+//
+contract('ETH payment', (accounts) =>{ 
+
+    // Check balance of account
+    it("Test get balance", async () =>{
+        const instance = await Ico.deployed();
+
+        console.log("deployed address:" +  instance.address);
+        console.log("A different address:" + accounts[1])
+        let balance = await web3.eth.getBalance(accounts[1]);
+        console.log("Address balance before:" +  web3.utils.fromWei(balance,"ether"));
+        //web3.utils.fromWei(balance1, "ether")
+    })
+
+
+    // Test buy tokens and check token balance.
+    it("Sender should receive correct amount of tokens after sending eth", async () =>{
+        const instance = await Ico.deployed()
+        const ethereumAmountString = "10"
+        const expectedTokensBought = 100;
+        // Should call the fallback function
+        //and thus also the buyTokens function
+        await instance.send(web3.utils.toWei(ethereumAmountString,'ether'), {from: accounts[1]}) // convert 10 eth to wei
+        //Check the balance.
+        const tokenAmount = await instance.balances(accounts[1]) // will return a string
+        assert.equal(parseInt(tokenAmount),expectedTokensBought)
+        let balance = await web3.eth.getBalance(accounts[1]);
+        console.log("Amount of tokens Bought:" +  tokenAmount);
+        
+
+    })
 })
